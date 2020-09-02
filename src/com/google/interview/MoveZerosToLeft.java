8f53d38518e59bb72f9fd3e05b8ba76431fe3bf8
pplitzner <p.plitzner@posteo.de>
2020-09-02 18:17:36 +0200

    Add bubble sort solution

--- src/com/google/interview/MoveZerosToLeft.java
+++ src/com/google/interview/MoveZerosToLeft.java
@@ -6,14 +6,15 @@ import static util.ArrayUtil.*;
 /**
  * Move zeros to the left
  *
- * Move all zeros to the left of an array while maintaining its order.
+ * Given an integer array, move all elements that are 0 to the left while maintaining
+ * the order of other elements in the array. The array has to be modified in-place.
  */
 public class MoveZerosToLeft {
 
     public static void main(String[] args) {
         int[] a = generateArray(1000);
 //        int[] a = {13, 5, -4, 0 ,2};
-        System.out.println("Start array");
+        System.out.println("Start array (Move Zeros)");
         printArray(a);
 
         int[] result = moveZeros(a); // O(n)
@@ -21,6 +22,27 @@ public class MoveZerosToLeft {
         System.out.println("Result array");
         printArray(result);
 
+        System.out.println("Start array (In place modification)");
+        printArray(a);
+
+        result = bubbleSortZeros(a); // O(n)
+
+        System.out.println("Result array");
+        printArray(result);
+    }
+
+    private static int[] bubbleSortZeros(int[] a) {
+        for (int i = a.length-1; i >= 0; i--) {
+            if(a[i]==0){
+                // swap up to start
+                for (int j = i; j >0; j--) {
+                    int temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
+        return a;
     }
 
     private static int[] moveZeros(int[] a) {
